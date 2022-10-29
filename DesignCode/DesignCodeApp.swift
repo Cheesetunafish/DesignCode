65b7f4fbdd54bb233d3bd90c51065bdbc5312883
Cheesetunafish <1614312228@qq.com>
2022-10-29 22:26:12 +0800

    Initial commit

---
+++ DesignCode/DesignCodeApp.swift
@@ -0,0 +1,17 @@
+//
+//  DesignCodeApp.swift
+//  DesignCode
+//
+//  Created by 小艾同学 on 2022/10/29.
+//
+
+import SwiftUI
+
+@main
+struct DesignCodeApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
