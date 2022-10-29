505011d120b1de56e6a3f9ebb7a16a684d219b92
Cheesetunafish <1614312228@qq.com>
2022-10-29 23:51:24 +0800

    BasicUI

--- DesignCode/ContentView.swift
+++ DesignCode/ContentView.swift
@@ -9,15 +9,41 @@ import SwiftUI
 
 struct ContentView: View {
     var body: some View {
-        VStack {
-            Image(systemName: "globe")
-                .imageScale(.large)
-                .foregroundColor(.accentColor)
-            Text("Hello, world!")
+        VStack(alignment: .leading, spacing: 8.0) {
+            Spacer()
+            Image("Logo 2")
+                .resizable(resizingMode: .stretch)
+                .aspectRatio(contentMode: .fit)
+                .frame(width: /*@START_MENU_TOKEN@*/26.0/*@END_MENU_TOKEN@*/, height: /*@START_MENU_TOKEN@*/26.0/*@END_MENU_TOKEN@*/)
+                .cornerRadius(20.0)
+            Text("SwiftUI for iOS 15")
+                .font( .largeTitle)
+                .fontWeight( .bold)
+            Text("20 sections - 3 hours".uppercased())
+                .font( .footnote)
+                .fontWeight( .semibold)
+                .foregroundColor( .secondary)
+            Text("Build an iOS app for iOS 15 with custom layouts, animations and ...")
+                .font( .footnote)
+                .multilineTextAlignment( .leading)
+                .lineLimit(2)
+                .frame(maxWidth: .infinity, alignment: .leading)
+                .foregroundColor( .secondary)
         }
-        .padding()
+        .padding( .all, 20.0)
+        .padding( .vertical, 20)
+        .frame(height: 350.0)
+        .background(Color("Background"))
+        .cornerRadius(30.0)
+        .shadow(color: Color("Shadow").opacity(0.3), radius: 10, x: 0, y: 10)
+        .padding( .horizontal, 20)
+        
     }
 }
+
+
+
+
 
 struct ContentView_Previews: PreviewProvider {
     static var previews: some View {
